29b7a0f5352d260874cc1fc03852a9451ec68b22
Pichaku123 <pribisht785@gmail.com>
2025-07-17 17:50:15 +0530

    localstorage done for projects, doing for todos now

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './styles.css';
 import { projects } from './project-list';
 import { renderSidebar, renderContent } from './display';
 
-const project1= projects.createProject("Project 1");
+projects.getFromLocal();    //load from localstorage before rendering sidebar so that projectlist is updated with whatever we had.
 
-
-renderSidebar();
-const allProjects= projects.getProjects();
-if(allProjects.length>0){
-    renderContent(allProjects[0]);
+let allProjects= projects.getProjects();
+if(allProjects.length === 0){   //had to change cuz we're already getting some stuff from localstorage
+    projects.createProject("Project 1");    //basically project 1 is only made if there's nothing in localstorage.
+    allProjects=projects.getProjects();
 }
+
+renderSidebar();        //duh
+renderContent(allProjects[0]);
 
 console.log("compiling finished tehehe");
--- src/project-list.js
+++ src/project-list.js
@@ -4,9 +4,47 @@ import { ProjectItem } from "./project-item";
 const projects= (() => {    //used factory function+IIFe cuz we only need 1 instance of projectlist.
     let projectList=[];
     
+    const saveToLocal= () => {
+        localStorage.setItem("todos", JSON.stringify(projectList));
+    }
+
+    const getFromLocal= () =>{
+        const storedData=localStorage.getItem("todos");
+        if(!storedData) return;     //in case no data is stored
+        const data=JSON.parse(storedData);
+        const newProjectList=[];    //store new data here, then replace old one with this one
+        //data is basically the projectList here.
+        //data has several projects which each have several todos.
+        //basically add the stuff we have in localstorage into projectlist when we open site.
+        //btw data will be a array of objects so yeah normal array methods work
+        for(let project of data)    //again, for/of cuz array of objects
+        {
+            const newProject= new ProjectItem(project.projectTitle);    //each project is instance of ProjectItem.
+            //oh also i gotta write code for the methods again cuz they're gone lol, JSON doesn't store methods soooo gg.
+            //so we gotta set the properties by ourselves as well lmaooo
+            for(let todos of project.todoList)
+            {
+                //for each "todo(not actual todo, but just the data)", we need to make an actual object in new array.
+                //we just have the strings, but the actual newProject has no data on it, so we need to set it manually.
+                newProject.addTodo(
+                    todos.title,
+                    todos.desc,
+                    todos.dueDate,
+                    todos.prio,
+                    todos.complete,
+                ) 
+            }
+            newProjectList.push(newProject);
+            //keep pushing to this, and fill it with previous projects from storage.
+        }
+        //now that list is done, set project list to this one.
+        projectList=newProjectList;
+    }
+
     const createProject = (title) => {
         const project= new ProjectItem(title);
         projectList.push(project);
+        saveToLocal();  //save after any changes are made to projectlist basically
         return project;
     }
 
@@ -20,9 +58,10 @@ const projects= (() => {    //used factory function+IIFe cuz we only need 1 inst
             return project.projectTitle===title;
         })
         projectList.splice(position, 1);
+        saveToLocal();      //same logic as in createproject.
     }
 
-    return {getProjects, createProject, removeProject};
+    return {getProjects, createProject, removeProject, saveToLocal, getFromLocal};
 })();
 
 
